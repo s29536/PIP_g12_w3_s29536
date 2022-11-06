aea52b24338fb47d5eea6cf9c61e9cc9e6ecb0e6
Sandra Socha <s29536@pjwstk.edu.pl>
2022-11-06 09:16:11 +0100

    new project

---
+++ src/Zadanie03/Main.java
@@ -0,0 +1,25 @@
+package src.Zadanie03;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int x;
+        int y;
+        int a;
+        int b;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj x i nacisnij enter");
+        x = scan.nextInt();
+        System.out.println("Podaj y i nacisnij enter");
+        y = scan.nextInt();
+        System.out.println("Podaj a i nacisnij enter");
+        a = scan.nextInt();
+        System.out.println("Podaj b i nacisnij enter");
+        b = scan.nextInt();
+        boolean alt = 
+        System.out.println(alt);
+
+
+    }
+}
